e86eab729eed041db03b1844b4100b053998609d
Joe Winger <josephwinger1@gmail.com>
2018-03-07 21:58:51 -0500

    Add current net worth

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ var JSONurl = 'https://forbes400.herokuapp.com/api/forbes400/real-time';
 $.getJSON(JSONurl, (data) => {
    $('#name').text(data[0]['name']);
    $('#title').text(data[0]['title']);
-   //console.log(data[0]);
+   $('#info').text("Current Net Worth: $" + (Math.round(data[0]['realTimeWorth'] / 100) / 10) + " B");
+   console.log(data[0]);
 });
