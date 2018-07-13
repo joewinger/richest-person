ffd5938867cae6718c3b6850f612c601656daa37
Joe Winger <josephwinger1@gmail.com>
2018-07-13 15:25:23 -0400

    Limit API call to 1 result

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var JSONurl = 'https://forbes400.herokuapp.com/api/forbes400/real-time';
+var JSONurl = 'https://forbes400.herokuapp.com/api/forbes400/real-time?limit=1';
 $.getJSON(JSONurl, (data) => {
    $('#name').text(data[0]['name']);
    $('#title').text(data[0]['title']);
