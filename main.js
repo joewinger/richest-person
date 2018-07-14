7ff55c8d5906d3eb7f53010c84e429a8edf7c289
Joe Winger <josephwinger1@gmail.com>
2018-07-14 00:33:34 -0400

    Save JSON results to variables

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var JSONurl = 'https://forbes400.herokuapp.com/api/forbes400/real-time?limit=1';
 $.getJSON(JSONurl, (data) => {
-   $('#name').text(data[0]['name']);
-   $('#title').text(data[0]['title']);
-   $('#info').text("Current Net Worth: $" + (Math.round(data[0]['realTimeWorth'] / 100) / 10) + " B");
+   var name = data[0]['name'];
+   var title = data[0]['title'];
+   var worth = "$" + (Math.round(data[0]['realTimeWorth'] / 100) / 10) + " B";
+   $('#name').text(name);
+   $('#title').text(title);
+   $('#info').text("Current Net Worth: " + worth);
    console.log(data[0]);
 });
