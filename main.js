a0b6cc900d4dbc9096a1c567873042b5b20bf037
Joe Winger <josephwinger1@gmail.com>
2018-07-14 00:49:07 -0400

    Add vue functionality

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 var JSONurl = 'https://forbes400.herokuapp.com/api/forbes400/real-time?limit=1';
+
+var app = new Vue({
+   el: '#app',
+   data: {
+      name: "",
+      title: "",
+      worth: ""
+   }
+});
+
 $.getJSON(JSONurl, (data) => {
    var name = data[0]['name'];
    var title = data[0]['title'];
    var worth = "$" + (Math.round(data[0]['realTimeWorth'] / 100) / 10) + " B";
-   $('#name').text(name);
-   $('#title').text(title);
-   $('#info').text("Current Net Worth: " + worth);
+
+   Vue.set(app, 'name', name);
+   Vue.set(app, 'title', title);
+   Vue.set(app, 'worth', worth);
+
    console.log(data[0]);
 });
