1700bb17e2b95b972ad0cf998c78df2ea9c40821
Joe Winger <josephwinger1@gmail.com>
2021-10-21 13:14:40 -0400

    Tidy up code, add new font

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Who Is The Richest Person Alive?
 [![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=SLPWNPB7AA9G2&lc=US&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted)
 
-Live-updating state-of-the-art display of the name of the richest person alive using cutting-edge web technologies.
+Live-updating, state-of-the-art global wealth tracker leveraging cutting-edge web technologies. /s
 
-Note: I didn't do anything cool for this. I just used an open source API to grab data and threw it on an HTML page. :)
+Pulls the richest person from Forbes using [This API](https://github.com/jesseokeya/Forbes400).
+
+Pretty simple app built with [Vue 2](https://vuejs.org/) and the [Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API).
--- main.js
+++ main.js
@@ -3,20 +3,20 @@ var JSONurl = 'https://forbes400.herokuapp.com/api/forbes400/real-time?limit=1';
 var app = new Vue({
    el: '#app',
    data: {
-      name: "",
-      title: "",
-      worth: ""
+      name: localStorage['name'] || "",
+      source: localStorage['source'] || "",
+      worth: localStorage['worth'] || ""
+   },
+   async created() {
+      const response = await fetch(JSONurl);
+      let data = await response.json();
+      data = data[0];
+      this.name = data['personName'];
+      this.source = data['source'];
+      this.worth = "$" + (Math.round(data['finalWorth'] / 100) / 10) + "B";
+
+      localStorage['name'] = this.name;
+      localStorage['source'] = this.source;
+      localStorage['worth'] = this.worth;
    }
-});
-
-$.getJSON(JSONurl, (data) => {
-   var name = data[0]['name'];
-   var title = data[0]['title'];
-   var worth = "$" + (Math.round(data[0]['realTimeWorth'] / 100) / 10) + "B";
-
-   Vue.set(app, 'name', name);
-   Vue.set(app, 'title', title);
-   Vue.set(app, 'worth', worth);
-
-   console.log(data[0]);
 });
